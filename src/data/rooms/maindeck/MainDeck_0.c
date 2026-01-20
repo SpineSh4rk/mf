61c3403e059aa99828901d416f1665124337e7b4
SpineSh4rk <spineshark@Pantheon.localdomain>
2026-01-20 23:04:58 +0100

    guh

--- src/data/rooms/maindeck/MainDeck_0.c
+++ src/data/rooms/maindeck/MainDeck_0.c
@@ -2,7 +2,7 @@
 #include "macros.h"
 
 // main deck rooms
-static const u8 sBlob_46f2f4_482a0c[] = INCBIN_U8("Blob_46f2f4_482a0c.bin");
+static const u8 sBlob_46f2f4_482a0c[] = INCBIN_U8("data/Blob_46f2f4_482a0c.bin");
 
-static const u8 sBlob_482a0c_575a60[] = INCBIN_U8("Blob_482a0c_575a60.bin");
+static const u8 sBlob_482a0c_575a60[] = INCBIN_U8("data/Blob_482a0c_575a60.bin");
 
