04f847f1663289afe114f19078215cf00d9ad6e3
clue cat <gitlab@clue.cat>
2026-02-15 12:31:04 -0500

    decompile electric_water_damage_box2

--- src/sprites_AI/electric_water_damage_box2.c
+++ src/sprites_AI/electric_water_damage_box2.c
@@ -4,6 +4,7 @@
 #include "data/sprite_data.h"
 
 #include "constants/sprite.h"
+#include "constants/event.h"
 
 #include "structs/sprite.h"
 
@@ -30,5 +31,25 @@ void ElectricWaterDamageBox2Stopping(void)
 }
 
 void ElectricWaterDamageBox2(void) {
+    switch (gCurrentSprite.pose) {
+    case SPRITE_POSE_UNINITIALIZED:
+        ElectricWaterDamageBox2Init();
+        break;
 
+    case SPRITE_POSE_IDLE:
+        ElectricWaterDamageBox2Idle();
+        break;
+
+    case 24:
+        ElectricWaterDamageBox2DamagingSamus();
+        break;
+
+    case 26:
+        ElectricWaterDamageBox2Stopping();
+        break;
+    }
+
+    if (EVENT_EFFECT_80 & gCurrentEventBasedEffect) {
+        gCurrentSprite.status = 0;
+    }
 }
