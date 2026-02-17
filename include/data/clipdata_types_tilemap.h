e0232b5b7134791ff37af4af38c4a5ddb2b3b7ef
SpineShark <249215007+SpineSh4rk@users.noreply.github.com>
2026-02-18 00:28:28 +0100

    Unblob sTilesetEntries array

---
+++ include/data/clipdata_types_tilemap.h
@@ -0,0 +1,18 @@
+#ifndef CLIPDATA_TYPES_TILEMAP_H
+#define CLIPDATA_TYPES_TILEMAP_H
+
+#include "types.h"
+
+extern const u8 sClipdataCollisionTypes[];
+
+extern const u8 sClipdataCollisionTypes_Test[];
+
+extern const u8 sTileset_0_Tilemap[1604];
+
+extern const u16 sTileset_0_Pal[14 * 16];
+
+extern const u8 sClipdataBehaviorTypes[];
+
+extern const u8 sClipdataBehaviorTypes_Test[];
+
+#endif /* CLIPDATA_TYPES_TILEMAP_H */
